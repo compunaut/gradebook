2aaab5e7f9c63b92705215f7b520102e572d9ecd
ESIS668 <mustangracer256@gmail.com>
2020-04-18 16:17:12 +0000

    Initital web app setup with route to login page that contains credential functionality

--- gradebook.py
+++ gradebook.py
@@ -1,11 +1,35 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="ESIS668",
+    password="Kon@bik",
+    hostname="ESIS668.mysql.pythonanywhere-services.com",
+    databasename="ESIS668$668gradebook",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# Route for handling the login page logic
+@app.route('/sign_in', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'professor1' or request.form['password'] != 'umbc123':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('class_selection.html'))
+    return render_template('sign_in.html', error=error)
 
